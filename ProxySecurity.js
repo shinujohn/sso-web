e79bc2a59c6984845c55145129292c2746e09b39
Shinu John <shinu.john@capita.co.uk>
2015-01-20 03:05:14 +0530

    Initial Commit

---
+++ ProxySecurity.js
@@ -0,0 +1,86 @@
+﻿
+var adal = require('adal-node');
+var q = require('q');
+var crypto = require('crypto');
+var fs = require("fs");
+
+var ProxySecurity = function () {
+    
+    /*
+    * General configuration
+    */
+    var config = {
+        tenant: 'aadonesc.onmicrosoft.com',
+        loginUrl: 'https://login.windows.net',
+        clientId: 'c5d1f887-0348-42e2-9f49-cdc733af53ef',
+        clientSecret: 'ihtijQhEkdwvoZGPbjg3QXbNMzrEiS64g2cfvIRZc7Y='
+    };
+    
+    ///* NOT USED IN WEBAPP
+    
+    
+    // * Retrieves the access token for the given key from Azure AD
+    // */
+    //this.getAccessToken = function (key) {
+    //    var deferred = q.defer();
+    
+    //    var AuthenticationContext = adal.AuthenticationContext;
+    //    var loginUrl = (process.env.loginUrl || config.loginUrl) + '/' + (process.env.tenant || config.tenant);
+    
+    //    var redirectUri = (process.env.redirectUrl || config.redirectUrl);
+    //    var resource = (process.env.resource || config.resource);
+    
+    //    var authenticationContext = new AuthenticationContext(loginUrl);
+    //    authenticationContext.acquireTokenWithAuthorizationCode(
+    //        key, 
+    //        redirectUri, 
+    //        resource, 
+    //        (process.env.clientId || config.clientId), 
+    //        (process.env.clientSecret || config.clientSecret), 
+    //        function (err, response) {
+    //            if (!err) {
+    //                var claimsToken = getClaimsToken(response.userId);
+    //                var signedToken = signJSON(claimsToken);
+    //                deferred.resolve(signedToken);
+    //            }
+    //            else {
+    //                deferred.reject(err);
+    //            }
+    //        });
+    
+    //    return deferred.promise;
+    //}
+    
+    /**
+     * Validates the signature of the token
+     */
+    this.validateToken = function (token) {
+        var data = token.split('.')[0];
+        var signature = token.split('.')[1];
+        var publickey = fs.readFileSync('osc.pub').toString();
+        
+        var isValid = crypto.createVerify('RSA-SHA256').update(data).verify(publickey, signature, 'base64');
+        return isValid;
+    }
+    
+    ///* NOT USED IN WEBAPP
+    // * Sign
+    // */
+    //function signJSON(jsonString) {
+    //    var privatekey = fs.readFileSync('osc.pem').toString();
+    //    var data = new Buffer(JSON.stringify(jsonString)).toString('base64');
+    //    var signature = crypto.createSign('RSA-SHA256').update(data).sign(privatekey, 'base64');
+    //    return data + '.' + signature;
+    //}
+    
+    //// Test data
+    //// This will be returned by the organisation service
+    //function getClaimsToken(userId) {
+    //    return {
+    //        'userId': userId,
+    //        'roles': ['admin', 'socialworker']
+    //    };
+    //}
+}
+
+module.exports = new ProxySecurity();
---
+++ server.js
@@ -0,0 +1,84 @@
+﻿var express = require('express');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var path = require('path');
+var proxySecurity = require('./ProxySecurity');
+
+var server = express();
+
+server.use(cookieParser());
+
+server.set('views', __dirname + '/public/views');
+server.engine('html', require('ejs').renderFile);
+server.set('view engine', 'ejs');
+
+server.use(session({
+    name: 'session',
+    secret: 'd26fb56d-aa1f-4722-a892-0788557021ba',
+    rolling : true, 
+    resave: false,
+    saveUninitialized: true
+}));
+
+/*
+ * General configuration
+ * loginUrl: application will be redirected to this url if the request is not authenticated
+ */
+var config = {
+    loginUrl: "http://localhost:8788/auth/login"
+
+};
+
+/*
+ * Handler to recieve the token from API once successfully authenticated
+ */
+server.get('/auth', function (req, res) {
+    var token = req.query.token;
+    if (token) {
+        res.cookie('auth', token);
+        req.user = parseUserDataFromToken(token);
+        session.user = parseUserDataFromToken(token);
+        res.redirect('/');
+    }
+    else {
+        res.send('error - token not recieved');
+    }
+});
+
+/*
+ * Middleware to validate user context/authentication status
+ */
+server.use(function (req, res, next) {
+    if (req.cookies.auth && proxySecurity.validateToken(req.cookies.auth)) {
+        next();
+    }
+    else {
+        res.redirect(process.env.loginUrl || config.loginUrl);
+    }
+});
+
+/*
+ * Root
+ */
+server.get('/', function (req, res) {
+    res.render('./index.html', { title: 'Express' });
+});
+
+/*
+ * Returns general configuration data
+ */
+server.get('/config', function (req, res) {
+    res.send({
+        apiBaseUrl: (process.env.apiBaseUrl || 'http://localhost:8788')
+    });
+});
+
+
+server.listen(process.env.port || 8787, function () {
+    console.log('Webapp started');
+});
+
+// private utility
+function parseUserDataFromToken(token) {
+    return (new Buffer(token.split('.')[0], 'base64')).toString('ascii');
+}
